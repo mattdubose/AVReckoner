28a98378400c3fdbfdee5e0287234784bb1641b4
mattdubose <matt_dubose@hotmail.com>
2025-09-18 09:40:34 -0400

    added AccountHoldingsPage

---
+++ Views/AccountHoldingsPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Reckoner.Views;
+
+public partial class AccountHoldingsPage : UserControl
+{
+    public AccountHoldingsPage()
+    {
+        InitializeComponent();
+    }
+}
